a845ca171a2098cf3ac29d8c76d8b2cfd2d2c4bb
KSalmaze <105123464+KSalmaze@users.noreply.github.com>
2024-02-29 01:14:32 +0000

    Falta 3

--- Pratica1.sql
+++ Pratica1.sql
@@ -1,24 +1,115 @@
-CREATE TABLE Federacao(
-    Nome VARCHAR2(256) PRIMARY KEY,
+-- Pedro Henrique Salmaze 13783714
+CREATE TABLE Federacao(--Federacao 
+    Nome VARCHAR2(256),
+        CONSTRAINT PK_NOME PRIMARY KEY (Nome),
     Data_Fundacao DATE NOT NULL
 );
 
-CREATE TABLE Nacao(
-    Nome VARCHAR2(256) PRIMARY KEY,
+CREATE TABLE Nacao(--Nacao
+    Nome VARCHAR2(256),
+        CONSTRAINT PK_NOME PRIMARY KEY (Nome),
     Quantidade_Planetas NUMBER(10),
     Federacao VARCHAR2(256),
         CONSTRAINT fk_federacao FOREIGN KEY (Federacao) REFERENCES Federacao(Nome)
 );
 
-CREATE TABLE Nacao_Facao(
+CREATE TABLE Dominacia(
+    Nacao VARCHAR2(256),
+        CONSTRAINT FK_NACAO FOREIGN KEY (Nacao) REFERENCES Nacao(Nome),
+    Planeta VARCHAR2(256),
+        CONSTRAINT FK_PLANETA FOREIGN KEY (Planeta) REFERENCES Planeta(Designacao_Astronomica),
+    Data_ini DATE,
+        CONSTRAINT PK_DOMINACIA PRIMARY KEY (Nacao, Planeta, Data_ini) ,
+    Data_fim DATE
+);
 
+CREATE TABLE Especie(
+    Nome_Cientifico VARCHAR2(256),
+        CONSTRAINT PK_NOME_CIENTIFICO PRIMARY KEY (Nome_Cientifico),
+    Planeta_Origem VARCHAR2(256) NOT NULL,
+        CONSTRAINT FK_PLANETA_ORIGEM FOREIGN KEY (Planeta_Origem) REFERENCES Planeta(Designacao_Astronomica),
+    Eh_Inteligente VARCHAR2(3) NOT NULL,
+        CHECK (LOWER(Eh_Inteligente) IN ('Sim', 'Nao'))-- Testar se funciona
 );
 
 CREATE TABLE Lider(
-    CPI VARCHAR2(100) PRIMARY KEY,
+    CPI VARCHAR2(100),
+        CONSTRAINT PK_LIDER PRIMARY KEY (CPI),
     Nome VARCHAR2(256),
     Cargo VARCHAR2(256) NOT NULL,
     Nacao VARCHAR2(256),
-        CONSTRAINT fk_nacao FOREIGN KEY (Nacao) REFERENCES Nacao(Nome)
-    Especie VARCHAR2(256),
+        CONSTRAINT fk_nacao FOREIGN KEY (Nacao) REFERENCES Nacao(Nome),
+    Especie VARCHAR2(256) NOT NULL,
+        CONTRAINT FK_ESPECIE FOREIGN KEY (Especie) REFERENCES Especie(Nome_Cientifico)
 );
+
+CREATE TABLE Faccao(
+    Nome VARCHAR2(256),
+        CONSTRAINT PK_NOME PRIMARY KEY (Nome),
+    Lider VARCHAR2(100),
+        CONSTRAINT FK_LIDER FOREIGN KEY (Lider) REFERENCES Lider(CPI),
+    Ideologia VARCHAR2(256),
+    Qnt_Nacoes NUMBER(10)
+);
+
+CREATE TABLE NacaoFaccao(
+    Nacao VARCHAR2(256),
+        CONSTRAINT FK_NACAO FOREIGN KEY (Nacao) REFERENCES Nacao(Nome),
+    Faccao VARCHAR2(256),
+        CONSTRAINT FK_FACCAO FOREIGN KEY (Faccao) REFERENCES Faccao(Nome),
+        CONSTRAINT PK_NACAO_FACCAO PRIMARY KEY (Nacao, Faccao)
+);
+
+CREATE TABLE Sistema(--Sistema
+    Estrela NUMBER(10),
+        CONSTRAINT FK_ESTRELA FOREIGN KEY (Estrela) REFERENCES Estrela(Id_Catalogo),
+        CONSTRAINT PK_ESTRELA PRIMARY KEY (Estrela),
+    Nome VARCHAR2(256)
+);
+
+CREATE TABLE Estrela(--Estrela
+    Id_Catalogo NUMBER(10),
+        CONSTRAINT PK_ID_CATALOGO PRIMARY KEY (Id_Catalogo),
+    Nome VARCHAR2(256),
+    Classificacao VARCHAR2(256),
+    Massa VARCHAR2(100), -- VARCHAR porque o numero pode ser muito grande
+    Coordenada_X NUMBER(10) NOT NULL,
+    Coordenada_Y NUMBER(10) NOT NULL,
+    Coordenada_Z NUMBER(10) NOT NULL,
+        CONSTRAINT UK_COORDENADAS UNIQUE (Coordenada_X, Coordenada_Y, Coordenada_Z)
+);
+
+CREATE TABLE OrbitaEstrela(--OrbitaEstrela
+    Orbitante NUMBER(10),
+        CONSTRAINT FK_ORBITANTE FOREIGN KEY (Orbitante) REFERENCES Estrela(Id_Catalogo),
+    Orbitada NUMBER(10),
+        CONSTRAINT FK_ORBITADA FOREIGN KEY (Orbitada) REFERENCES Estrela(Id_Catalogo),
+        CONSTRAINT PK_ORBITA PRIMARY KEY (Orbitante, Orbitada),
+    Distancia_Min VARCHAR2(100), --VARCHAR porque o numero pode ser muito grande
+    Distancia_Max VARCHAR2(100), --VARCHAR porque o numero pode ser muito grande
+    Periodo VARCHAR2(100) --VARCHAR porque o numero pode ser muito grande e possuir unidades de medida diferentes (ex: anos, dias, etc)
+);
+
+CREATE TABLE OrbitaPlameta(--
+    Planeta NUMBER(10),
+        CONSTRAINT FK_PLANETA FOREIGN KEY (Planeta) REFERENCES Planeta(Id_Catalogo),
+    Estrela NUMBER(10),
+        CONSTRAINT FK_ESTRELA FOREIGN KEY (Estrela) REFERENCES Estrela(Id_Catalogo),
+        CONSTRAINT PK_ORBITA PRIMARY KEY (Planeta, Estrela),
+    Distancia_Min VARCHAR2(100), --VARCHAR porque o numero pode ser muito grande
+    Distancia_Max VARCHAR2(100), --VARCHAR porque o numero pode ser muito grande
+    Periodo VARCHAR2(100) --VARCHAR porque o numero pode ser muito grande e possuir unidades de medida diferentes (ex: anos, dias, etc)
+);
+
+CREATE TABLE Planeta(
+    Designacao_Astronomica VARCHAR2(256),
+        CONSTRAINT PK_DESIGNACAO PRIMARY KEY (Designacao_Astronomica),
+    Massa VARCHAR2(100), --VARCHAR porque o numero pode ser muito grande
+    Raio VARCHAR2(100), --VARCHAR porque o numero pode ser muito grande
+    Composicao VARCHAR2(256),
+    Classicacao VARCHAR2(256)
+);
+
+CREATE TABLE Comunidade(
+    
+)

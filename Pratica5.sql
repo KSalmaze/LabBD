0d734839ae031bb18c5c18d3f8e127fd14412655
KSalmaze <105123464+KSalmaze@users.noreply.github.com>
2024-04-04 00:15:51 +0000

    1

--- Pratica5.sql
+++ Pratica5.sql
@@ -18,7 +18,8 @@ INSERT INTO Lider (CPI, Nome    , Cargo, Nacao, Especie)
 
 INSERT INTO Lider (CPI, Nome, Cargo, Nacao, Especie)
     VALUES ('132.456.789-13', 'Silvia', 'OFICIAL', 'Cordantes', 'Id illum fugit');
-    
+INSERT INTO Lider (CPI, Nome, Cargo, Nacao, Especie)
+    VALUES ('562.456.789-13', 'Pedro', 'OFICIAL', 'Cordantes', 'Id illum fugit');
 INSERT INTO Faccao (Nome, Lider, Ideologia, QTD_NACOES)
     VALUES ('Vanklion', '132.456.789-12', 'TOTALITARIA', 3);
 
@@ -30,5 +31,27 @@ INSERT INTO Faccao (Nome, Lider, Ideologia, QTD_NACOES)
 
 CREATE VIEW FaccoesProgressistas AS
     SELECT Nome, Lider FROM FACCAO 
-        WHERE Ideologia = 'Progressista'
+        WHERE Ideologia = 'PROGRESSITA'
             WITH READ ONLY;
+
+
+-- a)
+SELECT COUNT(*) FROM
+    FaccoesProgressistas;
+
+-- b)
+INSERT INTO FaccoesProgressistas 
+    (Nome, Lider)
+    VALUES ('Pedrinos', '562.456.789-13');
+
+-- Erro dado:
+/*
+Relatório de erros -
+Erro de SQL: ORA-42399: não é possível efetuar uma operação de DML em uma view somente para leitura
+42399.0000 - "cannot perform a DML operation on a read-only view"
+
+
+Esse erro ocorre pois a view foi criada com a cláusula WITH READ ONLY, 
+que impede a realização de operações de DML (Data Manipulation Language) como INSERT, UPDATE e DELETE.
+*/
+--

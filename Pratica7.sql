75639c0b0a06cbaeaf4bc3f44dc7d74b8a65e21f
dcbogni <163359349+dcbogni@users.noreply.github.com>
2024-04-24 20:11:25 -0300

    Update Pratica7.sql

--- Pratica7.sql
+++ Pratica7.sql
@@ -5,4 +5,29 @@
 DECLARE 
     v_istrela estrela.id_estrela%TYPE;
     v_
+
+DECLARE
+    v_max_num_estrelas NUMBER;
+    v_nome_estrela VARCHAR2(100);
+    v_id_estrela NUMBER;
     
+    CURSOR c_estrelas IS
+        SELECT nome, id, COUNT(*) AS num_estrelas_orbitantes
+        FROM estrelas_orbitantes
+        GROUP BY nome, id
+        ORDER BY COUNT(*) DESC;
+BEGIN
+    OPEN c_estrelas;
+    FETCH c_estrelas INTO v_nome_estrela, v_id_estrela, v_max_num_estrelas;
+    
+    DBMS_OUTPUT.PUT_LINE('Estrelas com o maior número de estrelas orbitantes:');
+    DBMS_OUTPUT.PUT_LINE('Maior número de estrelas orbitantes: ' || v_max_num_estrelas);
+    DBMS_OUTPUT.PUT_LINE('------------------------');
+    
+    WHILE c_estrelas%FOUND AND v_max_num_estrelas = (SELECT MAX(num_estrelas_orbitantes) FROM estrelas_orbitantes) LOOP
+        DBMS_OUTPUT.PUT_LINE('Nome: ' || v_nome_estrela || ', ID: ' || v_id_estrela);
+        FETCH c_estrelas INTO v_nome_estrela, v_id_estrela, v_max_num_estrelas;
+    END LOOP;
+    
+    CLOSE c_estrelas;
+END;

0bf55dd6064e39475a3225f44c306f3f16aeef6c
KSalmaze <105123464+KSalmaze@users.noreply.github.com>
2024-04-25 00:35:44 +0000

    1

--- Pratica7.sql
+++ Pratica7.sql
@@ -2,32 +2,41 @@
 -- Daniela Cristina Bogni - 11218577
 
 -- 1)
-DECLARE 
-    v_istrela estrela.id_estrela%TYPE;
-    v_
+-- Dados para teste
+INSERT INTO ORBITA_ESTRELA (orbitante, orbitada) VALUES ('GJ 9798','Zet1Mus'); 
+INSERT INTO ORBITA_ESTRELA (orbitante, orbitada) VALUES ('GJ 9798','GJ 2033'); 
+INSERT INTO ORBITA_ESTRELA (orbitante, orbitada) VALUES ('GJ 2033','Zet1Mus'); 
+
+set serveroutput on;
 
 DECLARE
     v_max_num_estrelas NUMBER;
-    v_nome_estrela VARCHAR2(100);
-    v_id_estrela NUMBER;
+    v_numero_orbitantes NUMBER;
+    v_nome_estrela orbita_estrela.orbitada%type;
+    v_id_estrela estrela.id_estrela%TYPE;
     
     CURSOR c_estrelas IS
-        SELECT nome, id, COUNT(*) AS num_estrelas_orbitantes
-        FROM estrelas_orbitantes
-        GROUP BY nome, id
+        SELECT O.orbitante, E.id_estrela, COUNT(*) AS num_estrelas_orbitantes
+        FROM orbita_estrela O JOIN estrela E
+        ON o.orbitada = e.nome
+        GROUP BY O.orbitante, E.id_estrela
         ORDER BY COUNT(*) DESC;
 BEGIN
+    dbms_output.put_line('Hello World!');
     OPEN c_estrelas;
     FETCH c_estrelas INTO v_nome_estrela, v_id_estrela, v_max_num_estrelas;
     
-    DBMS_OUTPUT.PUT_LINE('Estrelas com o maior número de estrelas orbitantes:');
-    DBMS_OUTPUT.PUT_LINE('Maior número de estrelas orbitantes: ' || v_max_num_estrelas);
-    DBMS_OUTPUT.PUT_LINE('------------------------');
+    v_numero_orbitantes := v_max_num_estrelas;
     
-    WHILE c_estrelas%FOUND AND v_max_num_estrelas = (SELECT MAX(num_estrelas_orbitantes) FROM estrelas_orbitantes) LOOP
-        DBMS_OUTPUT.PUT_LINE('Nome: ' || v_nome_estrela || ', ID: ' || v_id_estrela);
-        FETCH c_estrelas INTO v_nome_estrela, v_id_estrela, v_max_num_estrelas;
+    DBMS_OUTPUT.PUT_LINE('------------------------------------');
+    DBMS_OUTPUT.PUT_LINE('Maior número de estrelas orbitantes: ' || v_max_num_estrelas);
+    
+    WHILE c_estrelas%FOUND AND v_numero_orbitantes = v_max_num_estrelas LOOP
+        dbms_output.put_line(v_nome_estrela);
+        FETCH c_estrelas INTO v_nome_estrela, v_id_estrela, v_numero_orbitantes;
     END LOOP;
+    
+    DBMS_OUTPUT.PUT_LINE('------------------------------------');
     
     CLOSE c_estrelas;
 END;

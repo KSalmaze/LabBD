2d8a129b605759ae23ecf1c4a9948471abe34731
KSalmaze <105123464+KSalmaze@users.noreply.github.com>
2024-05-06 21:44:16 +0000

    Dados

--- Pratica8.sql
+++ Pratica8.sql
@@ -4,14 +4,27 @@
 */
 
 -- 1)
-SELECT Comunidade.*
-    FROM Comunidade
-    JOIN Habitacao ON Comunidade.Especie = Habitacao.Especie AND Comunidade.Nome = Habitacao.NomeEspecie
-    JOIN Dominancia ON Habitacao.Planeta = Dominancia.Planeta
-    JOIN NacaoFaccao ON Dominancia.Nacao = NacaoFaccao.Nacao
-    WHERE NacaoFaccao.Faccao = v_faccao
-      AND NOT EXISTS (
-        SELECT 1
-        FROM Participa
-        WHERE Participa.Especie = Comunidade.Especie AND Participa.NomeEspecie = Comunidade.Nome AND Participa.Faccao = v_faccao
-      );
+-- Dados para teste
+INSERT INTO NACAO_FACCAO VALUES('Sit id ipsam.','Vanklion');
+INSERT INTO NACAO_FACCAO VALUES('Aut explicabo.','Vetistas');
+INSERT INTO NACAO_FACCAO VALUES('Natus ut rem.','Jorgistas');
+INSERT INTO NACAO_FACCAO VALUES('Ad iure libero.','Mad');
+INSERT INTO NACAO_FACCAO VALUES('Ut minima.','FoG');
+
+INSERT INTO DOMINANCIA (PLANETA, NACAO, DATA_INI) VALUES('K2-290 b', 'Sit id ipsam.', TO_DATE('25/04/2190', 'DD/MM/YYYY'));
+INSERT INTO DOMINANCIA (PLANETA, NACAO, DATA_INI) VALUES('Kepler-696 b', 'Aut explicabo.', TO_DATE('25/04/2090', 'DD/MM/YYYY'));
+INSERT INTO DOMINANCIA (PLANETA, NACAO, DATA_INI) VALUES('Kepler-220 b', 'Natus ut rem.', TO_DATE('29/04/2190', 'DD/MM/YYYY'));
+INSERT INTO DOMINANCIA (PLANETA, NACAO, DATA_INI) VALUES('HD 96992 b', 'Ad iure libero.', TO_DATE('25/04/2600', 'DD/MM/YYYY'));
+INSERT INTO DOMINANCIA (PLANETA, NACAO, DATA_INI) VALUES('K2-62 c', 'Ut minima.', TO_DATE('25/06/2140', 'DD/MM/YYYY'));
+
+INSERT INTO COMUNIDADE (ESPECIE, NOME, QTD_HABITANTES) VALUES ('Quam aut', 'Comunidade 1', 15);
+INSERT INTO COMUNIDADE (ESPECIE, NOME, QTD_HABITANTES) VALUES ('Ipsa illo', 'Comunidade 2', 60);
+INSERT INTO COMUNIDADE (ESPECIE, NOME, QTD_HABITANTES) VALUES ('Fuga id', 'Comunidade 3', 12);
+INSERT INTO COMUNIDADE (ESPECIE, NOME, QTD_HABITANTES) VALUES ('Ad ab iure', 'Comunidade 4', 13);
+INSERT INTO COMUNIDADE (ESPECIE, NOME, QTD_HABITANTES) VALUES ('Ad quod', 'Comunidade 5', 22);
+
+INSERT INTO HABITACAO (PLANETA, ESPECIE, COMUNIDADE, DATA_INI) VALUES ('K2-290 b', 'Quam aut', 'Comunidade 1', TO_DATE('25/04/2190', 'DD/MM/YYYY'));
+INSERT INTO HABITACAO (PLANETA, ESPECIE, COMUNIDADE, DATA_INI) VALUES ('Kepler-696 b', 'Ipsa illo', 'Comunidade 2', TO_DATE('25/04/2090', 'DD/MM/YYYY'));
+INSERT INTO HABITACAO (PLANETA, ESPECIE, COMUNIDADE, DATA_INI) VALUES ('Kepler-220 b','Fuga id', 'Comunidade 3', TO_DATE('29/04/2190', 'DD/MM/YYYY'));
+INSERT INTO HABITACAO (PLANETA, ESPECIE, COMUNIDADE, DATA_INI) VALUES ('HD 96992 b','Ad ab iure', 'Comunidade 4', TO_DATE('25/04/2600', 'DD/MM/YYYY'));
+INSERT INTO HABITACAO (PLANETA, ESPECIE, COMUNIDADE, DATA_INI) VALUES ('K2-62 c','Ad quod', 'Comunidade 5', TO_DATE('25/06/2140', 'DD/MM/YYYY'));

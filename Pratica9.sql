4a358ff261a1af2072817c31e89bf056df3c612b
KSalmaze <105123464+KSalmaze@users.noreply.github.com>
2024-05-09 00:52:15 +0000

    Exercicio 2

--- Pratica9.sql
+++ Pratica9.sql
@@ -43,7 +43,7 @@ BEGIN
     RETURN v_distancia;
 END;
 
--- Programa PL/SQL
+-- Programa PL/SQL para testar
 DECLARE 
     v_estrela1 estrela%ROWTYPE;
     v_estrela2 estrela%ROWTYPE;
@@ -62,3 +62,58 @@ BEGIN
 END;
 
 -- 2)
+
+-- Declaracao do pacote
+-- Pacote acessavel por lideres
+CREATE OR REPLACE PACKAGE Funcoes_Lider AS
+    
+    e_Acesso_Negado EXCEPTION;
+    
+    PROCEDURE Remover_Nacao (
+     lider_logado IN lider%ROWTYPE,
+        v_nacao_a_ser_removida IN nacao.nome%TYPE);
+    
+END Funcoes_Lider;
+
+-- Corpo do pacote
+CREATE OR REPLACE PACKAGE BODY Funcoes_Lider AS
+
+-- Parametros: Lider logado e a nacao que deseja remover
+PROCEDURE Remover_Nacao(
+    lider_logado IN lider%ROWTYPE,
+    v_nacao_a_ser_removida IN nacao.nome%TYPE) AS
+    v_lider_esperado lider.Cpi%TYPE;
+    v_faccao_lider faccao.nome%TYPE;
+    
+BEGIN
+    SELECT F.Lider INTO v_lider_esperado FROM 
+        Nacao_Faccao NF JOIN Faccao F ON NF.Faccao = F.Nome
+        WHERE NF.Nacao = v_nacao_a_ser_removida;
+        
+    SELECT F.Nome INTO v_faccao_lider FROM
+        Faccao F JOIN Lider L ON F.Lider = L.Cpi WHERE L.Nome = lider_logado.Nome;
+
+    IF lider_logado.Cpi = v_lider_esperado THEN
+        DELETE FROM NACAO_FACCAO WHERE Nacao = v_nacao_a_ser_removida AND Faccao = v_faccao_lider;
+    ELSE
+        RAISE e_Acesso_Negado;
+    END IF;
+END;
+
+END Funcoes_Lider;
+
+-- Programa PL/SQL para testar
+DECLARE
+    v_lider lider%ROWTYPE;
+    v_nacao_para_remocao nacao.nome%TYPE;
+BEGIN
+    v_nacao_para_remocao := 'Sit id ipsam.';
+    
+    SELECT * INTO v_lider FROM Lider WHERE Nome = 'Oliver';
+    
+    Funcoes_Lider.Remover_Nacao(v_lider, v_nacao_para_remocao);
+EXCEPTION
+    WHEN Funcoes_Lider.e_Acesso_Negado THEN dbms_output.put_line('Acesso negado');
+END;
+
+-- 3)

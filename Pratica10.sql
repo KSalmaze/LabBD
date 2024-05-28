47255abaf8f82b30841b49c4a9027295aa737572
KSalmaze <105123464+KSalmaze@users.noreply.github.com>
2024-05-28 19:35:25 +0000

    1c

--- Pratica10.sql
+++ Pratica10.sql
@@ -6,23 +6,85 @@
 -- 1)
 -- a)
 /*
-    A ideia inicial era fazer essa verificação quando uma nova federação fosse inserida,
-    porém sempre que uma nova federação é inserida necessáriemte ela não vai estar associana a
-    nenhuma nação.
+    Um problema encontrado é que uma federação sempre vai ser inserida sem estar associada a nenhuma nação.
+    para passar por esse problema, podemos fazer algumas verificações como, verificar se há
+    pelo menos uma nação que pode ser associada a nova federação, e quando uma nação for 
+    deletada / atualizada verificar se não vai deixar nenhuma federação sem nação associada.
+*/ 
 
-    Explicar pq não vai ser perfeita, não importa a situação
-*/
 CREATE OR REPLACE TRIGGER Verficacao_Federacao
 BEFORE INSERT ON Federacao
 
+DECLARE
+    v_nacoes_sem_federacao NUMBER;
 BEGIN
-    
-    DELETE FROM Federacao WHERE Nome IN (SELECT Nome FROM Federacao
-                                            MINUS SELECT F.Nome FROM
-                                            Federacao F JOIN Nacao N ON
-                                            F.Nome = N.Federacao);
-    
+    SELECT COUNT(*) INTO v_nacoes_sem_federacao FROM
+        Nacao WHERE Federacao = NULL;
+        
+    IF v_nacoes_sem_federacao = 0 THEN
+        RAISE_APPLICATION_ERROR(-20001, 'A federacao nao pode ser criada, pois nao existe nenhuma nacao ');
+    END IF;
 END;
 
 
+
+
 -- b)
+
+-- c)
+
+CREATE OR REPLACE TRIGGER Atualizar_Nrm_Nacoes_em_Faccoes
+AFTER INSERT OR UPDATE OR DELETE ON Nacao_Faccao
+
+DECLARE
+    v_qntreal_nacoes NUMBER;
+    v_qntregistrada_nacoes NUMBER;
+BEGIN
+    -- foreach
+    FOR v_faccao IN (SELECT * FROM Faccao) 
+    LOOP
+        SELECT COUNT(*) INTO v_qntreal_nacoes 
+            FROM Nacao_Faccao WHERE Faccao = v_faccao.Nome;
+        
+        SELECT Qtd_Nacoes INTO v_qntregistrada_nacoes
+            FROM Faccao WHERE Nome = v_faccao.Nome;
+            
+        IF v_qntreal_nacoes != v_qntregistrada_nacoes THEN
+            UPDATE Faccao SET Qtd_Nacoes = v_qntreal_nacoes
+                WHERE Nome = v_faccao.Nome;
+        END IF;
+        
+    END LOOP;
+    
+END Atualizar_Nrm_Nacoes_em_Faccoes;
+
+-- Testando
+
+-- Select para determinar a quantidade de nações associadas a facção 'Vetistas'
+-- a saida foi 5, valor dado na inserção de vetistas na tabela facção
+SELECT * FROM Faccao WHERE Nome = 'Vetistas';
+
+-- Inserindo uma nova nacao na faccao
+INSERT INTO Nacao_Faccao (Faccao, Nacao) VALUES('Vetistas', 'Quis optio.'); 
+
+-- Repetindo o Select agora a quantidade de nacoes é 2, o valor correto considerando que vetistas
+-- ja tinha uma nacao associada a ela anteriormente
+SELECT * FROM Faccao WHERE Nome = 'Vetistas';
+
+-- Agora deletando uma faccao
+DELETE FROM Nacao_Faccao WHERE Nacao = 'Quis optio.';
+
+-- Repetindo o Select agora a quantidade de nacoes é 1, o valor correto
+
+-- Agora só falta testar uma atualização
+-- Para isso inserimos novamente a nacao e depois atualizamos a faccao associada
+INSERT INTO Nacao_Faccao (Faccao, Nacao) VALUES('Vetistas', 'Quis optio.'); 
+
+-- Agora o select informa 2 nacoes associadas a faccao vetistas
+
+UPDATE Nacao_Faccao SET Faccao = 'Mad' WHERE Nacao = 'Quis optio.';
+
+-- Agora informa apenas uma
+-- com isso o trigger funciona corretamente
+
+-- d)
